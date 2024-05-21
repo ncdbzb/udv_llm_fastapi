7d13e9afe42bedd294c971b1d58e592ffdca77c0
Konstantin Pershin <kostya.pershin.18@mail.ru>
2024-05-22 03:01:42 +0500

    upgrade patch users/me

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,5 +1,5 @@
 import jwt
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from fastapi import Depends, Request, Response, HTTPException, status
 from fastapi_users import BaseUserManager, exceptions, IntegerIDMixin, models, schemas, InvalidPasswordException
@@ -19,6 +19,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
 
     async def on_after_register(self, user: AuthUser, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
+
         if user.email != 'admin@admin.com':
             async with async_session_maker() as session:
                 await add_admin_request(user.email, session=session)
@@ -26,7 +27,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
             await send_email(name=user.name, user_email=user.email, token='', destiny='approval')
 
     async def on_after_forgot_password(
-        self, user: AuthUser, token: str, request: Optional[Request] = None
+            self, user: AuthUser, token: str, request: Optional[Request] = None
     ):
         print(f"User {user.id} has forgot his password")
         token_data = {
@@ -42,7 +43,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
         await send_email(user.name, user.email, token, 'forgot')
 
     async def validate_password(
-        self, password: str, user: AuthUser
+            self, password: str, user: AuthUser, user_update: Dict[str, Any] = None
     ) -> None:
         """
         Validate a password.
@@ -55,19 +56,30 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
         :return: None if the password is valid.
         """
         if len(password) < 6:
-            raise InvalidPasswordException('The password must be at least 6 characters long')
+            raise InvalidPasswordException(reason='The password must be at least 6 characters long')
         try:
             if password != user.confirmation_password:
                 raise InvalidPasswordException(reason="Passwords don't match")
         except AttributeError as e:
-            pass
+            if user_update:
+                try:
+                    if password != user_update['confirmation_password']:
+                        raise InvalidPasswordException(reason="Passwords don't match")
+                except KeyError:
+                    raise InvalidPasswordException(reason='Confirmation password is not filled')
+                try:
+                    if not self.password_helper.verify_and_update(
+                            user_update['old_password'], user.hashed_password)[0]:
+                        raise InvalidPasswordException(reason="Invalid password")
+                except KeyError:
+                    raise InvalidPasswordException(reason='Old password is not filled')
         return  # pragma: no cover
 
     async def create(
-        self,
-        user_create: schemas.UC,
-        safe: bool = False,
-        request: Optional[Request] = None,
+            self,
+            user_create: schemas.UC,
+            safe: bool = False,
+            request: Optional[Request] = None,
     ) -> models.UP:
         """
         Create a user in database.
@@ -93,7 +105,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
             if safe
             else user_create.create_update_dict_superuser()
         )
-        
+
         if user_dict["email"] == admin_dict["email"]:
             if user_dict["password"] == admin_dict["password"]:
                 user_dict = admin_dict.copy()
@@ -169,10 +181,10 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
         return verified_user
 
     async def on_after_login(
-        self,
-        user: models.UP,
-        request: Optional[Request] = None,
-        response: Optional[Response] = None,
+            self,
+            user: models.UP,
+            request: Optional[Request] = None,
+            response: Optional[Response] = None,
     ) -> None:
         """
         Perform logic after user login.
@@ -185,6 +197,54 @@ class UserManager(IntegerIDMixin, BaseUserManager[AuthUser, int]):
         Defaults to None
         """
         return  # pragma: no cover
+
+    async def update(
+            self,
+            user_update: schemas.UU,
+            user: models.UP,
+            safe: bool = False,
+            request: Optional[Request] = None,
+    ) -> models.UP:
+        """
+        Update a user.
+
+        Triggers the on_after_update handler on success
+
+        :param user_update: The UserUpdate model containing
+        the changes to apply to the user.
+        :param user: The current user to update.
+        :param safe: If True, sensitive values like is_superuser or is_verified
+        will be ignored during the update, defaults to False
+        :param request: Optional FastAPI request that
+        triggered the operation, defaults to None.
+        :return: The updated user.
+        """
+        if safe:
+            updated_user_data = user_update.create_update_dict()
+        else:
+            updated_user_data = user_update.create_update_dict_superuser()
+        updated_user = await self._update(user, updated_user_data)
+        await self.on_after_update(updated_user, updated_user_data, request)
+        return updated_user
+
+    async def _update(self, user: models.UP, update_dict: Dict[str, Any]) -> models.UP:
+        validated_update_dict = {}
+        for field, value in update_dict.items():
+            if field == "email" and value != user.email:
+                try:
+                    await self.get_by_email(value)
+                    raise exceptions.UserAlreadyExists()
+                except exceptions.UserNotExists:
+                    validated_update_dict["email"] = value
+                    # validated_update_dict["is_verified"] = False
+            elif field == "password" and value is not None:
+                await self.validate_password(value, user, update_dict)
+                validated_update_dict["hashed_password"] = self.password_helper.hash(
+                    value
+                )
+            elif field not in ('confirmation_password', 'old_password'):
+                validated_update_dict[field] = value
+        return await self.user_db.update(user, validated_update_dict)
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
