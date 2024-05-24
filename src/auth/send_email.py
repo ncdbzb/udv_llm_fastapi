5d38a8e403d90a00381336dc5bc9ae245a230907
Konstantin Pershin <kostya.pershin.18@mail.ru>
2024-05-24 12:25:05 +0500

    update verify link

--- src/auth/send_email.py
+++ src/auth/send_email.py
@@ -34,7 +34,7 @@ async def get_accepted_request_email_template(name: str, user_email: str, token:
         '<div>'
         f'<h1>Здравствуйте, {name}</h1>'
         '<p>Ваша заявка одобрена! Чтобы верифицировать аккаунт, перейдите по <b>ссылке</b></p>'
-        f'<p>https://{SERVER_DOMEN}/api/auth/verify/{token}</p>'
+        f'<p>https://localhost/logIn?token={token}</p>'
         '</div>',
         subtype='html'
     )
